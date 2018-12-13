806cb6ddc481c3017a36fd7fe58c5db3ed60f5eb
suttonau <sutton_mjbc@icloud.com>
2018-12-13 16:37:15 -0500

    setting up initial state for User login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import Profile from './Components/Profile';
+import LoginForm from './Components/LoginForm';
+import Nav from './Components/Nav';
+import NotFound from './Components/NotFound';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
+
+updateUser = (user) => {
+  this.setState({ currentUser: user })
+}
+
+componentDidMount(){
+  //see if there's a token
+  //send that token to the backend
+  //backend will send currentUser data
+
+  let token = localStorage.getItem('token')
+  if(token) {
+    fetch(`http://localhost:3000/api/v1/profile`, {
+      method: "GET",
+      headers: {
+        "Authentication" : `Bearer ${token}`
+      }
+    }).then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        currentUser: data.user
+      })
+    })
+  }
+}
+
+
+
   render() {
     return (
       <div className="App">
